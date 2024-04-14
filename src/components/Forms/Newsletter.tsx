4602e85411645ad686eb1033f6b1e717023fa3ea
AndyAshleyLV <andrew.ashley@leadventure.com>
2024-04-13 23:44:59 -0400

    removed unused imnport

--- src/components/Forms/Newsletter.tsx
+++ src/components/Forms/Newsletter.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Button, Form, InputGroup } from "react-bootstrap";
 import ThankYouModal from "../ThankYouModal/ThankYouModal";
 
